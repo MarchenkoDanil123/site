f3cbb5aeeee4d599bb60c540d7281857a2e3c92a
MarchenkoDanil123 <demarchenko@edu.hse.ru>
2023-08-18 14:49:27 +0300

    bdfbdfsbd

--- site/index.html+style.css /main.js
+++ site/index.html+style.css /main.js
@@ -17,17 +17,17 @@ dropdowns.forEach(dropdown => {
 document.addEventListener("DOMContentLoaded", function() {
     const centerTexts = document.querySelectorAll(".center-text");
 
-    const messages = ["Let's Discover", "ChatGPT", "Let's Collaborate", "Let's create"];
+    const messages = ["Let's Discover", "ChatGPT", "Let's Collaborate", "Let's create", "Let's design"];
     let currentMessageIndex = 0;
 
     function changeMessage() {
-        gsap.to(centerTexts[currentMessageIndex], { opacity: 0, y: -50, duration: 1.5, onComplete: function() {
+        gsap.to(centerTexts[currentMessageIndex], { opacity: 0, y: -50, duration: 2.5, onComplete: function() {
             centerTexts[currentMessageIndex].classList.add("hidden");
 
             currentMessageIndex = (currentMessageIndex + 1) % messages.length;
 
             centerTexts[currentMessageIndex].textContent = messages[currentMessageIndex];
-            gsap.to(centerTexts[currentMessageIndex], { opacity: 1, y: 0, duration: 1.5, delay: 0.2, onComplete: changeMessage });
+            gsap.to(centerTexts[currentMessageIndex], { opacity: 1, y: 0, duration: 2.5, delay: 0.2, onComplete: changeMessage });
             centerTexts[currentMessageIndex].classList.remove("hidden");
         } });
     }
