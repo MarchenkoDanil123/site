766bc91dd900e27d61a3c33e8dbb1a6743d7f4b9
MarchenkoDanil123 <demarchenko@edu.hse.ru>
2023-08-18 14:09:42 +0300

    мвыамыв

--- site/main.js
+++ site/main.js
@@ -9,3 +9,30 @@ dropdowns.forEach(dropdown => {
         dropdown.querySelector('.dropdown-content').style.display = 'none';
     });
 });
+
+
+
+// Анимация центр страницы
+
+document.addEventListener("DOMContentLoaded", function() {
+    const centerTexts = document.querySelectorAll(".center-text");
+
+    const messages = ["Let's Discover", "ChatGPT", "Let's Collaborate", "Let's create"];
+    let currentMessageIndex = 0;
+
+    function changeMessage() {
+        gsap.to(centerTexts[currentMessageIndex], { opacity: 0, y: -50, duration: 1.5, onComplete: function() {
+            centerTexts[currentMessageIndex].classList.add("hidden");
+
+            currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+
+            centerTexts[currentMessageIndex].textContent = messages[currentMessageIndex];
+            gsap.to(centerTexts[currentMessageIndex], { opacity: 1, y: 0, duration: 1.5, delay: 0.2, onComplete: changeMessage });
+            centerTexts[currentMessageIndex].classList.remove("hidden");
+        } });
+    }
+
+    changeMessage();
+});
+
+// ----------------------------------------------------------------------
